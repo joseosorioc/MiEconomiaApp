22bdfcbe6f8d35020be7b1aa782539f518f8923a
Jose Osorio Catalan <jossekarlos10@gmail.com>
2023-01-09 00:00:36 -0500

    feat (features):  add modal form and show data in datatable.

--- src/app/features/form-gasto/form-gasto.component.ts
+++ src/app/features/form-gasto/form-gasto.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CategoriasService } from '../../core/services/categorias.service';
+import { Categoria } from '../../core/models/Categoria.interface';
+import { Detalle } from '../../core/models/Detalle.interface';
+import { DetallesService } from '../../core/services/detalles.service';
+import { DetalleResponseInterface } from '../../core/models/response/DetalleResponseinterface';
 
 @Component({
   selector: 'form-gasto',
@@ -9,22 +14,40 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class FormGastoComponent implements OnInit {
 
   formIncomesOrExpenses!: FormGroup;
+  categories: Categoria [] = [];
+  details : DetalleResponseInterface = {} as DetalleResponseInterface ;
+  mostrarDialogAgregarDetalle: boolean = false;
 
-  constructor(private fb : FormBuilder) {
+
+
+  constructor(private fb : FormBuilder,
+              private categoriasService: CategoriasService,
+              private detalleService: DetallesService) {
 
      this.formIncomesOrExpenses = this.fb.group({
-        date: ['', Validators.required, Validators],
+        date: ['', [Validators.required] ],
         amount: [ '', Validators.required ],
         category: [ '', Validators.required ],
         account: [ '', Validators.required ],
         type: [ '', Validators.required ],
-        note: [ '', Validators.required, Validators.maxLength ]
+        note: [ '', [Validators.required, Validators.maxLength] ]
      });
    }
 
   ngOnInit(): void {
+    this.categoriasService.getCategorias().subscribe( categoriasData => {
+    this.categories = categoriasData;
+    });
+
+    this.detalleService.getDetalles().subscribe( detallesData => {
+    this.details = detallesData;
+    });
 
   }
 
+
+  agregarDetalle(){
+    this.mostrarDialogAgregarDetalle = true;
+  }
 
 }
