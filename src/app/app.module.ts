af56a60f1e21a2a140af84a774e9ca2b9683bd41
Jose Osorio Catalan <jossekarlos10@gmail.com>
2023-01-12 22:43:06 -0500

    use module httpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CardModule } from 'primeng/card';
-import { Button, ButtonModule } from 'primeng/button';
+import {  ButtonModule } from 'primeng/button';
 
 import { AppComponent } from './app.component';
 import { sidebarModule } from './shared/sidebar/sidebar.module';
 import { RoutingModule } from './routing.module';
 import { FeaturesModule } from './features/features.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -19,6 +20,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     sidebarModule,
     BrowserAnimationsModule,
     CardModule,
+    HttpClientModule,
     RoutingModule,
     FeaturesModule,
     ButtonModule
