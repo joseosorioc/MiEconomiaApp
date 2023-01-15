af0d048526c9228b752564be4bd94d6c02fc9c8b
Jose Osorio Catalan <jossekarlos10@gmail.com>
2023-01-14 22:28:19 -0500

    feat: add methods in service post and utils to form component.

--- src/app/core/services/detalles.service.ts
+++ src/app/core/services/detalles.service.ts
@@ -1,26 +1,39 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'environments/environment';
+import { SelectItem } from 'primeng/api';
 import { Observable } from 'rxjs';
+import { DetalleResponseInterface } from '../models/response/DetalleResponseinterface';
+import { IncomeOrExpenseEnum } from '../enums/IncomeOrExpense.enum';
+import { DetalleRequest } from '../models/requests/DetalleRequest.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DetallesService {
 
-  private apiURL = environment.url + "";
+  private apiURL = environment.url + "api/v1/detalle";
 
   constructor( private http: HttpClient ) {
 
   }
 
-  /*
-  public getDetalles() : Observable<any>
+  public getTypesDetails(): SelectItem[]
   {
-    this.
+    return Object.keys(IncomeOrExpenseEnum)
+              .map(key => ({ value: key , label: IncomeOrExpenseEnum[key] }))
+
   }
 
-*/
+  public getDetalles() : Observable<DetalleResponseInterface>
+  {
+    return this.http.get<DetalleResponseInterface>(`${this.apiURL}/detalles`);
+  }
+
+  public guardarNuevoDetalle(detalleRequest: DetalleRequest ): Observable<any> {
+    return this.http.post(`${this.apiURL}/detalle`, detalleRequest);
+  }
+
 
 
 
--- src/app/core/services/fuentes.service.ts
+++ src/app/core/services/fuentes.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
+import { Fuente } from '../models/Fuente.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FuentesService {
 
-  constructor() { }
+  private apiURL =  environment.url +  'api/v1/fuente';
+
+  constructor( private http: HttpClient) {
+  }
+
+
+
+  getAllFuentes(): Observable<Fuente[]>
+  {
+    return this.http.get<Fuente[]>(`${this.apiURL}/fuentes`);
+  }
+
+
 }
